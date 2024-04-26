51682622c9fba8ede43443ec7ec77532c4159ffb
Digman <long2513@gmail.com>
2024-04-26 16:54:03 +0800

    update

--- client.go
+++ client.go
@@ -18,47 +18,86 @@ type Client struct {
 	WindowSize   [2]int
 }
 
-var clientProfiles = map[string]map[string]tls_client.ClientProfile{
-	"Chrome": {
-		"default":    tls_client.DefaultClientProfile,
-		"Chrome/100": tls_client.Chrome_100,
-		"Chrome/101": tls_client.Chrome_100,
-		"Chrome/102": tls_client.Chrome_102,
-		"Chrome/103": tls_client.Chrome_103,
-		"Chrome/104": tls_client.Chrome_104,
-		"Chrome/105": tls_client.Chrome_105,
-		"Chrome/106": tls_client.Chrome_106,
-		"Chrome/107": tls_client.Chrome_107,
-		"Chrome/108": tls_client.Chrome_108,
-		"Chrome/109": tls_client.Chrome_109,
-		"Chrome/110": tls_client.Chrome_110,
-		"Chrome/111": tls_client.Chrome_111,
-		"Chrome/112": tls_client.Chrome_112,
-		"Chrome/113": tls_client.Chrome_117,
-		"Chrome/114": tls_client.Chrome_117,
-		"Chrome/115": tls_client.Chrome_117,
-		"Chrome/116": tls_client.Chrome_117,
-		"Chrome/117": tls_client.Chrome_117,
-		"Chrome/118": tls_client.Chrome_117,
-		"Chrome/119": tls_client.Chrome_117,
-		"Chrome/120": tls_client.Chrome_120,
-		"Chrome/121": tls_client.Chrome_120,
+type profileList struct {
+	term     string
+	defaults tls_client.ClientProfile
+	profiles map[string]tls_client.ClientProfile
+}
+
+var clientProfiles = []profileList{
+	{
+		term:     "Chrome",
+		defaults: tls_client.DefaultClientProfile,
+		profiles: map[string]tls_client.ClientProfile{
+			"Chrome/100": tls_client.Chrome_100,
+			"Chrome/101": tls_client.Chrome_100,
+			"Chrome/102": tls_client.Chrome_102,
+			"Chrome/103": tls_client.Chrome_103,
+			"Chrome/104": tls_client.Chrome_104,
+			"Chrome/105": tls_client.Chrome_105,
+			"Chrome/106": tls_client.Chrome_106,
+			"Chrome/107": tls_client.Chrome_107,
+			"Chrome/108": tls_client.Chrome_108,
+			"Chrome/109": tls_client.Chrome_109,
+			"Chrome/110": tls_client.Chrome_110,
+			"Chrome/111": tls_client.Chrome_111,
+			"Chrome/112": tls_client.Chrome_112,
+			"Chrome/113": tls_client.Chrome_112,
+			"Chrome/114": tls_client.Chrome_112,
+			"Chrome/115": tls_client.Chrome_112,
+			"Chrome/116": tls_client.Chrome_112,
+			"Chrome/117": tls_client.Chrome_117,
+			"Chrome/118": tls_client.Chrome_117,
+			"Chrome/119": tls_client.Chrome_117,
+			"Chrome/120": tls_client.Chrome_120,
+		},
 	},
-	"iPhone": {
-		"default": tls_client.Safari_IOS_16_0,
-		"OS 15_5": tls_client.Safari_IOS_15_5,
-		"OS 15_6": tls_client.Safari_IOS_15_6,
-		"OS 15_7": tls_client.Safari_IOS_15_6,
-		"OS 16_0": tls_client.Safari_IOS_16_0,
-		"OS 16_1": tls_client.Safari_IOS_16_0,
-		"OS 16_2": tls_client.Safari_IOS_16_0,
-		"OS 17_1": tls_client.Safari_IOS_17_0,
-		"OS 17_2": tls_client.Safari_IOS_17_0,
-		"OS 17_3": tls_client.Safari_IOS_17_0,
-		"OS 17_4": tls_client.Safari_IOS_17_0,
+	{
+		term:     "Firefox",
+		defaults: tls_client.Firefox_123,
+		profiles: map[string]tls_client.ClientProfile{
+			"Firefox/102": tls_client.Firefox_102,
+			"Firefox/104": tls_client.Firefox_104,
+			"Firefox/105": tls_client.Firefox_105,
+			"Firefox/106": tls_client.Firefox_106,
+			"Firefox/108": tls_client.Firefox_108,
+			"Firefox/109": tls_client.Firefox_108,
+			"Firefox/110": tls_client.Firefox_110,
+			"Firefox/111": tls_client.Firefox_110,
+			"Firefox/112": tls_client.Firefox_110,
+			"Firefox/113": tls_client.Firefox_110,
+			"Firefox/114": tls_client.Firefox_110,
+			"Firefox/115": tls_client.Firefox_110,
+			"Firefox/116": tls_client.Firefox_110,
+			"Firefox/117": tls_client.Firefox_117,
+			"Firefox/118": tls_client.Firefox_117,
+			"Firefox/119": tls_client.Firefox_117,
+			"Firefox/120": tls_client.Firefox_120,
+			"Firefox/121": tls_client.Firefox_120,
+			"Firefox/122": tls_client.Firefox_120,
+			"Firefox/123": tls_client.Firefox_123,
+		},
 	},
-	"iPad": {
-		"default": tls_client.Safari_Ipad_15_6,
+	{
+		term:     "Version",
+		defaults: tls_client.Safari_16_0,
+		profiles: map[string]tls_client.ClientProfile{
+			"Version/15": tls_client.Safari_15_6_1,
+		},
+	},
+	{
+		term:     "iPhone",
+		defaults: tls_client.Safari_IOS_16_0,
+		profiles: map[string]tls_client.ClientProfile{
+			"OS 15_5": tls_client.Safari_IOS_15_5,
+			"OS 15_6": tls_client.Safari_IOS_15_6,
+			"OS 16_":  tls_client.Safari_IOS_16_0,
+			"OS 17_":  tls_client.Safari_IOS_17_0,
+		},
+	},
+	{
+		term:     "iPad",
+		defaults: tls_client.Safari_Ipad_15_6,
 	},
 }
 
@@ -209,19 +248,17 @@ func newClient(userAgent string, windowSize [2]int, timeout int) *Client {
 
 func getClientProfile(userAgent string) tls_client.ClientProfile {
 	profile := tls_client.DefaultClientProfile
-	for ua, kv := range clientProfiles {
-		if strings.Contains(userAgent, ua) {
-			profile = kv["default"]
-			for s, clientProfile := range kv {
-				if s != "default" && strings.Contains(userAgent, s) {
-					profile = clientProfile
+	for _, clientProfile := range clientProfiles {
+		if strings.Contains(userAgent, clientProfile.term) {
+			profile = clientProfile.defaults
+			for k, v := range clientProfile.profiles {
+				if strings.Contains(userAgent, k) {
+					profile = v
 					break
 				}
 			}
-			break
 		}
 	}
-
 	return profile
 }
 
