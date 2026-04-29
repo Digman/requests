ce26f4ca90a428257263ad29045a549bd1baae88
digman <long2513@gmail.com>
2026-04-29 21:17:19 +0800

    update

--- client.go
+++ client.go
@@ -340,7 +340,7 @@ func (c *Client) GetFingerPrint() (bool, string) {
 }
 
 func (c *Client) GetIPLocation() (bool, string) {
-	_, b, e := c.NewRequest().Get("http://ip-api.com/json").Send().End()
+	_, b, e := c.NewRequest().Get("https://ipinfo.io/json").Send().End()
 	if e != nil {
 		return false, e.Error()
 	}
